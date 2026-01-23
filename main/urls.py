2234697e4dbc8da4905a9f872a4a2f34a65f6e58
David <d11218700@gmail.com>
2026-01-23 14:27:38 +0600

    Добавил страницу с новостями

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('apply/', views.apply, name='apply'),
+    path('about/', views.about, name='about'),
+    path('news/', views.news_list, name='news_list'),
+    path('jobs/', views.jobs, name='jobs'),
+    path('science/', views.science, name='science'),
+    path('courses/', views.courses, name='courses'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,5 +6,23 @@ def home(request):
     return render(request, 'main/home.html', {
         'last_news': last_news
     })
+def about(request):
+    return render(request, 'main/about.html')
 
+def apply(request):
+    return render(request, 'main/apply.html')
 
+def news_list(request):
+    news_list = News.objects.filter(is_active=True).order_by('-created_at')
+    return render(request, 'main/news.html', {
+        'news_list': news_list
+    })
+
+def jobs(request):
+    return render(request, 'main/jobs.html')
+
+def science(request):
+    return render(request, 'main/science.html')
+
+def courses(request):
+    return render(request, 'main/courses.html')
