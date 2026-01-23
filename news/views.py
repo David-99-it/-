81b512ac04f49374de58a602e1187b29a92fe51e
David <d11218700@gmail.com>
2026-01-23 20:03:57 +0600

    Отдел с новостями сделан

--- news/views.py
+++ news/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 def news_detail(request, pk):
-    news = get_object_or_404(News, pk=pk, is_active=True)
+    news = get_object_or_404(News, pk=pk)
+    referer = request.META.get('HTTP_REFERER', '/news/')  # если нет реферера, идем на /news/
+
+    # Берём якорь, если передали через GET
+    anchor = request.GET.get('anchor')
+    if anchor:
+        back_url = f"{referer}#{anchor}"
+    else:
+        back_url = referer
+
     return render(request, 'news/news_detail.html', {
-        'news': news
+        'news': news,
+        'back_url': back_url
     })
