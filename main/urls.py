42fc0b2a22bc732b80aee7d1abc649ba6de4b9ae
David <d11218700@gmail.com>
2026-01-25 11:49:50 +0600

    Добавлен минимальный поиск

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('news/', views.news_list, name='news_list'),
     path('jobs/', views.jobs, name='jobs'),
-     path('vacancies/apply/', views.vacancy_apply, name='vacancy_apply'),
+    path('vacancies/apply/', views.vacancy_apply, name='vacancy_apply'),
+    path('search/', views.search_results, name='search_results'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from news.models import News
 from jobs.models import Vacancy
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
 
 def home(request):
     last_news = News.objects.filter(is_active=True).order_by('-created_at')[:3]
@@ -31,3 +33,19 @@ def jobs(request):
 def vacancy_apply(request):
     # просто покажем страницу с формой Google
     return render(request, 'main/vacancy_apply.html')
+
+def search_results(request):
+    query = request.GET.get('q', '')
+    news_results = News.objects.filter(
+        Q(title__icontains=query) | Q(text__icontains=query)
+    ) if query else News.objects.none()
+    
+    vacancy_results = Vacancy.objects.filter(
+        Q(title__icontains=query) | Q(description__icontains=query)
+    ) if query else Vacancy.objects.none()
+
+    return render(request, 'main/search_results.html', {
+        'query': query,
+        'news_results': news_results,
+        'vacancy_results': vacancy_results,
+    })
